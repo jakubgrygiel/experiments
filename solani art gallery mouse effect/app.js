5e5b7bf8d2734b22edc890290a145a06aaab1c37
Jakub Grygiel <jakub.g.grygiel@gmail.com>
2023-03-14 22:49:23 +0100

    Add cursor to art gallery

--- solani art gallery mouse effect/app.js
+++ solani art gallery mouse effect/app.js
@@ -2,7 +2,21 @@ const body = document.querySelector("body");
 const imagesWrapper = document.querySelector(".images-wrapper");
 const imagesSlider = document.querySelector(".images-slider");
 const cursor = document.querySelector(".cursor");
+const cursorVisual = document.querySelector(".cursor-visual");
+const images = document.querySelectorAll(".image");
 
+const cursorTiming = {
+  duration: 300,
+  fill: "forwards",
+  easing: "linear",
+};
+const animTiming = {
+  duration: 1000,
+  fill: "forwards",
+  easing: "ease",
+};
+
+let cursorOverImage = false;
 let mousePosX;
 let mousePosY;
 let movePos;
@@ -33,23 +47,10 @@ function getImagesPosition() {
 body.addEventListener("mousemove", (e) => {
   getMousePos(e);
   getImagesPosition();
-
   const cursorKeyframes = {
     transform: `translate(${mousePosX - 16}px, ${mousePosY - 16}px)`,
   };
-  const cursorTiming = {
-    duration: 300,
-    fill: "forwards",
-    easing: "linear",
-  };
-
   const animKeyframes = { transform: `translateX(-${movePos}px)` };
-  const animTiming = {
-    duration: 1000,
-    fill: "forwards",
-    easing: "ease",
-  };
-
   cursor.animate(cursorKeyframes, cursorTiming);
   imagesSlider.animate(animKeyframes, animTiming);
 });
@@ -58,4 +59,13 @@ window.addEventListener("resize", () => {
   imagesWrapperWidth = imagesWrapper.clientWidth;
   windowX = window.innerWidth;
   h2wr.textContent = `${imagesWrapperWidth} / ${windowX}`;
+});
+
+images.forEach((image) => {
+  image.addEventListener("mouseenter", (e) => {
+    cursorVisual.classList.add("cursor-visual--over-image");
+  });
+  image.addEventListener("mouseleave", (e) => {
+    cursorVisual.classList.remove("cursor-visual--over-image");
+  });
 });
