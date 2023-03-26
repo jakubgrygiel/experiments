6b06cb2902411b9f056e084d0c383cfe6b728df9
Jakub Grygiel <jakub.g.grygiel@gmail.com>
2023-03-26 23:11:11 +0200

    Add experiment ala palette website

---
+++ palette gallery mouse effect/app.js
@@ -0,0 +1,60 @@
+const body = document.querySelector("body");
+const galleryWrapper = document.querySelector(".gallery-wrapper");
+const gallery = document.querySelector(".gallery");
+
+const cursorTiming = {
+  duration: 300,
+  fill: "forwards",
+  easing: "linear",
+};
+const animTiming = {
+  duration: 1000,
+  fill: "forwards",
+  easing: "ease",
+};
+
+let mousePosX;
+let mousePosY;
+let movePos;
+let galleryWrapperWidth = galleryWrapper.clientWidth;
+let galleryWidth = gallery.clientWidth;
+let galleryWrapperHeight = galleryWrapper.clientHeight;
+let galleryHeight = gallery.clientHeight;
+
+function getMousePos(e) {
+  mousePosX = e.pageX;
+  mousePosY = e.pageY;
+}
+
+function getImagesPosition() {
+  let posX = mousePosX / galleryWrapperWidth;
+  let posY = mousePosY / galleryWrapperHeight;
+  //   console.log(posX, posY);
+  movePosX = (galleryWidth - galleryWrapperWidth) * posX;
+  movePosY = (galleryHeight - galleryWrapperHeight) * posY;
+  console.log(movePosX, movePosY);
+  return [movePosX, movePosY];
+}
+
+body.addEventListener("mousemove", (e) => {
+  getMousePos(e);
+  getImagesPosition();
+  const animKeyframes = {
+    transform: `translate(-${movePosX}px,-${movePosY}px)`,
+  };
+  gallery.animate(animKeyframes, animTiming);
+});
+
+window.addEventListener("resize", () => {
+  galleryWrapperWidth = galleryWrapper.clientWidth;
+  galleryWrapperHeight = galleryWrapper.clientWidth;
+});
+
+// images.forEach((image) => {
+//   image.addEventListener("mouseenter", (e) => {
+//     cursorVisual.classList.add("cursor-visual--over-image");
+//   });
+//   image.addEventListener("mouseleave", (e) => {
+//     cursorVisual.classList.remove("cursor-visual--over-image");
+//   });
+// });
